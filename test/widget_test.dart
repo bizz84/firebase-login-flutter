887e54ab4db99483089b4ce63fabe11cd081c477
Andrea Bizzotto <bizz84@gmail.com>
2018-04-04 21:19:55 +0100

    Add widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,23 +7,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:login/main.dart';
+import 'package:login/login_page.dart';
+
+import 'package:login/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMock implements Auth {
+  bool didRequestSignIn = false;
+  bool didRequestCreateUser = true;
+  Future<FirebaseUser> signIn(String email, String password) async {
+    didRequestSignIn = true;
+    return null;
+  }
+  Future<FirebaseUser> createUser(String email, String password) async {
+    didRequestCreateUser = true;
+    return null;
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  Widget makeTestWidget(LoginPage loginPage) {
+    return new MediaQuery(
+      data: new MediaQueryData(),
+      child: new MaterialApp(home: loginPage)
+    );
+  }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('''
+    Given email and password are empty
+    When login button is pressed
+    Then sign in is called
+    ''', (WidgetTester tester) async {
+
+    AuthMock mock = new AuthMock();
+    LoginPage loginPage = new LoginPage(title: 'test', auth: mock);
+    await tester.pumpWidget(makeTestWidget(loginPage));
+
+    Finder loginButton = find.byKey(new Key('login'));
+    await tester.tap(loginButton);
+    
+    expect(mock.didRequestSignIn, false);
+  });
+
+  testWidgets('''
+    Given email and password are non-empty
+    When login button is pressed
+    Then sign is called
+    ''', (WidgetTester tester) async {
+
+    AuthMock mock = new AuthMock();
+    LoginPage loginPage = new LoginPage(title: 'test', auth: mock);
+    await tester.pumpWidget(makeTestWidget(loginPage));
+
+    Finder emailField = find.byKey(new Key('email'));
+    await tester.enterText(emailField, 'email');
+
+    Finder passwordField = find.byKey(new Key('password'));
+    await tester.enterText(passwordField, 'password');
+
+    Finder loginButton = find.byKey(new Key('login'));
+    await tester.tap(loginButton);
+    
+    expect(mock.didRequestSignIn, true);
   });
 }
